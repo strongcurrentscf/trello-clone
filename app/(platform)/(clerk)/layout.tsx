7688c02be48772add7e35778bd74859f41ae1500
Christian Francis <cfrancis1989@gmail.com>
2024-10-17 04:06:49 -0400

    install/config Clerk authentication

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const clerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100 flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default clerkLayout;
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Taskify",
+  description: "Collaborte, manage projects, and reach new productivity peaks.",
+};
